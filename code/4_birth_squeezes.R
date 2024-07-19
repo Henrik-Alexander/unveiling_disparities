71fb39a591ba304bb1d434f2e16b5c2c9d2bd7ab
Schubert <schubert@demogr.mpg.de>
2024-07-19 16:34:47 +0200

    Update the code for review and stable pop approach.

--- code/4_birth_squeezes.R
+++ code/4_birth_squeezes.R
@@ -25,7 +25,7 @@ fert <- fert[!is.na(fert$tfr_female) & !is.na(fert$tfr_male), ]
 
 ### Load the Schoumaker and Dudel data -----------------
 
-# Load the schoumaker data
+# Load the schoumaker data:
 # Data accessed from: https://perso.uclouvain.be/bruno.schoumaker/data/
 path_global <- "U:/data/global/"
 sd <- read_xlsx(paste0(path_global, "schoumaker_male/A. Estimates of male and female fertility.xlsx"), sheet = 1)
@@ -36,7 +36,7 @@ sd$data <- "Schoumaker's fertility estimates"
 sd[, c("tfr_male", "tfr_female")] <- apply(sd[, c("tfr_male", "tfr_female")], 2, as.numeric)
 sd <- sd[!is.na(sd$tfr_female) & !is.na(sd$tfr_male), ]
 
-# Load the male-fertility database
+# Load the male-fertility database:
 # Data accessed from: https://www.fertilitydata.org/Home/Index
 hfc_files <- list.files(paste0(path_global, "male_fertility_database"), full.names = T)
 hfc <- lapply(hfc_files, read.delim, sep = ",")
@@ -62,9 +62,9 @@ names(hfc) <- tolower(names(hfc))
 hfc$data <- "Human Fertility Collection"
 hfc <- hfc[!is.na(hfc$tfr_female) & !is.na(hfc$tfr_male), ]
 
-### Load the Schoen, 1985 data
+### Load the Schoen(1985) data:
 rsd <- read.csv("data/schoen_birthsqueeze_1985.csv")
-rsd$country <- gsub("^[0-I]+. ", "", rsd$country)
+rsd$country <- gsub("^[0-I]+. ", "", rsd$ï..country)
 rsd$tfr_ratio <- rsd$tfr_male / rsd$tfr_female
 rsd$data <- "Robert Schoen, 1985" 
 
@@ -151,8 +151,44 @@ pp <- t(rbind(pp, fert$tfr_ratio))
 fert$posterior_predict <- factor(ifelse(fert$tfr_ratio < pp[, 1] | fert$tfr_ratio > pp[, 2], 1, 0),
                                  labels = c("no squeeze", "birth squeeze"))
 
-### 3. Stable population approach ----------------------
+### 3. Review approach ----------------------
 
+# Compare the different datas-ource
+distr_tfr_ratio <- tapply(fert$tfr_ratio, fert$data,
+                          function(x) quantile(x, probs = c(0.1, 0.5,  0.9)), simplify = F)
+distr_tfr_ratio <- array2DF(distr_tfr_ratio)
+distr_tfr_ratio$N <- as.numeric(tapply(fert$tfr_ratio, fert$data, length))
+distr_tfr_ratio <- distr_tfr_ratio[, c("Var1", "N", "10%", "50%", "90%")]
+stargazer(distr_tfr_ratio, summary = F)
+
+# Estimate the diciles for the country-level data
+country_fert <- fert[fert$data != "Subnational Fertility Data", ]
+review_threshold <- quantile(country_fert$tfr_ratio, probs = c(0.1, 0.9))
+fert$review_approach <- factor(ifelse(fert$tfr_ratio < review_threshold[1] | fert$tfr_ratio > review_threshold[2], 1, 0), 
+labels = c("no squeeze", "birth squeeze"))
+
+
+### 4. Stable population approach -----------
+
+# Set the growth rate
+r <- 1
+
+# Create the grid
+stable_pop_df <- expand.grid(age_diff = seq(0, 1, by = 0.2),
+                             pop_diff = seq(0.9, 1.1, by = 0.1),
+                             srb = 1 / seq(1, 1.08, by = 0.01))
+stable_pop_df$tfr_ratio <- with(stable_pop_df, srb * pop_diff * exp(age_diff))
+stable_pop_th <- quantile(stable_pop_df$tfr_ratio, probs = c(0.1, 0.9))
+
+# Create the birth squeeze variable
+fert$stable_pop_approach <- factor(ifelse(fert$tfr_ratio < stable_pop_th[1] | fert$tfr_ratio > stable_pop_th[2], 1, 0), 
+                               labels = c("no squeeze", "birth squeeze"))
+
+
+### 5. Impact approach -----------------
+
+
+#### Include the results in a plot --------
 
 # Estimate the population squeeze for one region-year observation
 estimate_population_squeeze <- function(data) {
@@ -188,12 +224,12 @@ files <- files[!str_detect(files, "nat")]
 asfrs <- lapply(files, rda2list)
 
 
-
 ### Plot the results -----------------------------------
+
 
 # Plot the predictions
 plot_birth_squeeze <- function(data = fert, approach = naive_approach, label = "Naive approach") {
-  p <- ggplot(data = subset(data, data == "subnational fertility data"),
+  p <- ggplot(data = subset(data, data == "Subnational Fertility Data"),
               aes(x = tfr_female, y = tfr_male, colour = {{approach}}, shape = {{approach}})) +
     geom_abline(intercept = 0, slope = 1) +
     geom_point(alpha = .5) +
@@ -210,15 +246,17 @@ plot_birth_squeeze <- function(data = fert, approach = naive_approach, label = "
 ###
 naive <- plot_birth_squeeze()
 empiric <- plot_birth_squeeze(approach = posterior_predict, label = "Empiric approach")
+review <- plot_birth_squeeze(approach = review_approach, label = "Review approach")
+stable <- plot_birth_squeeze(approach = stable_pop_approach, label = "Stable population approach")
 
 # Assembel the plot
-naive + empiric + 
+(naive + review) / (empiric + stable) +
   plot_layout(guides = "collect")
 ggsave(last_plot(), filename = "figures/birth_squeezes_panel.pdf", height = 15, width = 20, unit = "cm")
 
 
 naive <- fert |> 
-  filter(data == "subnational fertility data") |>
+  filter(data == "Subnational Fertility Data") |>
   group_by(country) |> 
   mutate(total = n()) |> 
   group_by(country, naive_approach) |> 
@@ -234,7 +272,7 @@ naive <- fert |>
 
 
 empiric <- fert |> 
-  filter(data == "subnational fertility data") |>
+  filter(data == "Subnational Fertility Data") |>
   group_by(country) |> 
   mutate(total = n()) |> 
   group_by(country, posterior_predict) |> 
@@ -250,16 +288,33 @@ empiric <- fert |>
 
 naive + empiric + 
   plot_layout(guides = "collect")
+ggsave(last_plot(), filename = "figures/share_birth_squeeze.pdf")
 
+#  Plot the distribution of the different TFR ratios 
+fert |> 
+  select(data, tfr_ratio) |> 
+  ggplot(aes(x = tfr_ratio, fill = data)) +
+  geom_histogram(colour = "white") +
+  geom_vline(xintercept = 1, colour = "red", linetype = "dashed", linewidth = 1.2) +
+  facet_wrap(~ data, scales = "free_y") +
+  scale_y_continuous("Number of region-year observations", expand = c(0, 0)) +
+  scale_x_continuous("TFR ratio", expand = c(0, 0), n.breaks = 10, trans = "log") +
+  scale_fill_viridis_d() +
+  guides(fill = "none") +
+  theme(
+    strip.text = element_text(size = 10)
+  )
+ggsave(last_plot(), filename = "figures/distribution_tfr_datasets.pdf", height = 15, width = 20, unit = "cm")
 
 # Plot the data
 ggplot(fert, aes(x = tfr_female, y = tfr_male, colour = data, shape = data)) + 
   geom_abline(intercept = 0, slope = 1) +
-  geom_point(alpha = .3) +
+  geom_point(alpha = .5) +
   scale_x_continuous("TFR female", limits = c(0, 10), n.breaks = 10, expand = c(0, 0)) +
   scale_y_continuous("TFR male", limits = c(0, 10), n.breaks = 10, expand = c(0, 0)) +
   theme(legend.position = c(0.65, 0.2)) +
   scale_colour_viridis_d(name = "Data source:") + 
+  scale_shape(name = "Data source:") +
   guides(colour = guide_legend(override.aes = list(alpha = 1, size = 3)))
 
 ggsave(last_plot(), filename = "figures/data_sources.pdf", height = 15, width = 15, unit = "cm")
